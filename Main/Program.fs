2fc5bc6e47bc93a726170ff6d4f3a679c8bd44d5
Aron <aronadler@hotmail.com>
2019-05-12 18:44:45 +0100

    Add project files.

---
+++ Main/Program.fs
@@ -0,0 +1,8 @@
+﻿open System
+
+open Library
+
+[<EntryPoint>]
+let main _ =
+    printfn "Individuals: %A" (getIndividuals())
+    0 // return an integer exit code
---
+++ SQLStuff/Library.fs
@@ -0,0 +1,32 @@
+﻿module Library
+
+open FSharp.Data.Sql
+
+open System.Data.Common
+
+// Connect to a public DB (https://rfam.readthedocs.io/en/latest/database.html)
+[<Literal>]
+let connString = "Server=mysql-rfam-public.ebi.ac.uk;Database=Rfam;User=rfamro;Port=4497"
+
+[<Literal>]
+let dbVendor = Common.DatabaseProviderTypes.MYSQL
+
+[<Literal>]
+let indivAmount = 1000
+
+[<Literal>]
+let useOptTypes = true
+
+type sql = SqlDataProvider<
+            dbVendor,
+            connString,
+            //ResolutionPath = resPath,
+            IndividualsAmount = indivAmount,
+            UseOptionTypes = useOptTypes>
+
+let ctx = sql.GetDataContext()
+
+let getIndividuals _ = 
+    ctx.Rfam.FamilyLong.Individuals
+    //|> Seq.map (fun e -> e.ColumnValues |> Seq.toList)
+    //|> Seq.toList
